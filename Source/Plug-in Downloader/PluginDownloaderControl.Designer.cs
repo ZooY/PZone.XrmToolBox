2a63ef6bf16ff0ee37a067e5a1c3decd6644c864
Roman Kopaev <roman@kopaev.ru>
2018-02-15 11:46:09 +0300

    no message

---
+++ Source/Plug-in Downloader/PluginDownloaderControl.Designer.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using XrmToolBox.Extensibility;
+
+
+namespace PZone.XrmToolBox
+{
+    partial class PluginDownloaderControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PluginDownloaderControl));
+            this.NameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.VersionColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PluginsList = new System.Windows.Forms.ListView();
+            this.IdColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.MainCommandBar = new System.Windows.Forms.ToolStrip();
+            this.DownloadButton = new System.Windows.Forms.ToolStripButton();
+            this.SaveAssemblyDialog = new System.Windows.Forms.SaveFileDialog();
+            this.CloseButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.AboutDropDownButton = new System.Windows.Forms.ToolStripDropDownButton();
+            this.AboutButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.MainCommandBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.DisplayIndex = 0;
+            this.NameColumn.Text = "Name";
+            this.NameColumn.Width = 294;
+            // 
+            // VersionColumn
+            // 
+            this.VersionColumn.DisplayIndex = 1;
+            this.VersionColumn.Text = "Version";
+            // 
+            // PluginsList
+            // 
+            this.PluginsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.IdColumn,
+            this.NameColumn,
+            this.VersionColumn});
+            this.PluginsList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PluginsList.FullRowSelect = true;
+            this.PluginsList.Location = new System.Drawing.Point(0, 0);
+            this.PluginsList.MultiSelect = false;
+            this.PluginsList.Name = "PluginsList";
+            this.PluginsList.Size = new System.Drawing.Size(941, 540);
+            this.PluginsList.TabIndex = 0;
+            this.PluginsList.UseCompatibleStateImageBehavior = false;
+            this.PluginsList.View = System.Windows.Forms.View.Details;
+            this.PluginsList.SelectedIndexChanged += new System.EventHandler(this.PluginsList_SelectedIndexChanged);
+            // 
+            // IdColumn
+            // 
+            this.IdColumn.DisplayIndex = 2;
+            this.IdColumn.Text = "ID";
+            this.IdColumn.Width = 0;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.PluginsList);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(941, 540);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(941, 565);
+            this.toolStripContainer1.TabIndex = 1;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.MainCommandBar);
+            // 
+            // MainCommandBar
+            // 
+            this.MainCommandBar.Dock = System.Windows.Forms.DockStyle.None;
+            this.MainCommandBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CloseButton,
+            this.toolStripSeparator1,
+            this.AboutDropDownButton,
+            this.toolStripSeparator2,
+            this.DownloadButton});
+            this.MainCommandBar.Location = new System.Drawing.Point(3, 0);
+            this.MainCommandBar.Name = "MainCommandBar";
+            this.MainCommandBar.Size = new System.Drawing.Size(197, 25);
+            this.MainCommandBar.TabIndex = 0;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Image = ((System.Drawing.Image)(resources.GetObject("DownloadButton.Image")));
+            this.DownloadButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(81, 22);
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.ToolTipText = "Download selected Plug-in on disk";
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // SaveAssemblyDialog
+            // 
+            this.SaveAssemblyDialog.DefaultExt = "dll";
+            this.SaveAssemblyDialog.Filter = "DLL (*.dll)|*.dll|All files (*.*)|*.*";
+            this.SaveAssemblyDialog.InitialDirectory = "C:\\Users\\ra.kopaev\\Desktop";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.CloseButton.Image = ((System.Drawing.Image)(resources.GetObject("CloseButton.Image")));
+            this.CloseButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(23, 22);
+            this.CloseButton.Text = "Close";
+            this.CloseButton.ToolTipText = "Close this tab";
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // AboutDropDownButton
+            // 
+            this.AboutDropDownButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutButton});
+            this.AboutDropDownButton.Image = ((System.Drawing.Image)(resources.GetObject("AboutDropDownButton.Image")));
+            this.AboutDropDownButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.AboutDropDownButton.Name = "AboutDropDownButton";
+            this.AboutDropDownButton.Size = new System.Drawing.Size(69, 22);
+            this.AboutDropDownButton.Text = "About";
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(152, 22);
+            this.AboutButton.Text = "About plug-in";
+            this.AboutButton.Click += new System.EventHandler(this.AboutButton_Click);
+            // 
+            // PluginDownloaderControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "PluginDownloaderControl";
+            this.Size = new System.Drawing.Size(941, 565);
+            this.Load += new System.EventHandler(this.PluginDownloaderControl_Load);
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.MainCommandBar.ResumeLayout(false);
+            this.MainCommandBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColumnHeader NameColumn;
+        private System.Windows.Forms.ColumnHeader VersionColumn;
+        private System.Windows.Forms.ListView PluginsList;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip MainCommandBar;
+        private System.Windows.Forms.ToolStripButton DownloadButton;
+        private System.Windows.Forms.ColumnHeader IdColumn;
+        private System.Windows.Forms.SaveFileDialog SaveAssemblyDialog;
+        private System.Windows.Forms.ToolStripButton CloseButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripDropDownButton AboutDropDownButton;
+        private System.Windows.Forms.ToolStripMenuItem AboutButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
---
+++ Source/Plug-in Downloader/PluginDownloaderControl.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Xrm.Sdk.Query;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
+
+
+namespace PZone.XrmToolBox
+{
+    public partial class PluginDownloaderControl : PluginControlBase, IGitHubPlugin//, IPayPalPlugin
+    {
+        public PluginDownloaderControl()
+        {
+            InitializeComponent();
+        }
+
+
+        private void DownloadButton_Click(object sender, System.EventArgs e)
+        {
+            var assemblyId = PluginsList.SelectedItems[0].SubItems[0].Text;
+            var assembly = Service.Retrieve("pluginassembly", new Guid(assemblyId), new ColumnSet("name", "content"));
+            var name = assembly.GetAttributeValue<string>("name");
+            var content = assembly.GetAttributeValue<string>("content");
+
+            SaveAssemblyDialog.FileName = name;
+            var dialog = SaveAssemblyDialog.ShowDialog();
+            if (dialog == DialogResult.OK)
+                File.WriteAllBytes(SaveAssemblyDialog.FileName, Convert.FromBase64String(content));
+        }
+
+
+        private void PluginDownloaderControl_Load(object sender, System.EventArgs ee)
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Retrieve Plug-ins...",
+                Work = (w, e) =>
+                {
+                    var query = "<fetch>" +
+                                "<entity name=\"pluginassembly\">" +
+                                "<attribute name=\"name\" />" +
+                                "<attribute name=\"version\" />" +
+                                "<filter>" +
+                                "<condition attribute=\"sourcetype\" operator=\"eq\" value=\"0\" />" +
+                                "<condition attribute=\"introducedversion\" operator=\"eq\" value=\"1.0\" />" +
+                                "</filter>" +
+                                "<order attribute=\"name\" />" +
+                                "</entity>" +
+                                "</fetch>";
+                    var assemblies = Service.RetrieveMultiple(new FetchExpression(query));
+                    e.Result = assemblies.Entities.Select(a => new ListViewItem(new[] { a.Id.ToString(), a.GetAttributeValue<string>("name"), a.GetAttributeValue<string>("version") })).ToArray();
+                },
+                PostWorkCallBack = e =>
+                {
+                    PluginsList.Items.AddRange((ListViewItem[])e.Result);
+                    PluginsList.Items[0].Selected = true;
+                },
+                MessageWidth = 340,
+                MessageHeight = 150
+            });
+        }
+
+
+        private void PluginsList_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            DownloadButton.Enabled = PluginsList.SelectedItems.Count > 0;
+        }
+
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            new AboutForm().ShowDialog();
+        }
+
+
+        public string RepositoryName => "PZone.XrmToolBox";
+        public string UserName => "ZooY";
+        public string DonationDescription => "Donation for MSCRM Tools - PZone Plug-in Downloader";
+        public string EmailAccount => "roman@kopaev.ru";
+    }
+}
