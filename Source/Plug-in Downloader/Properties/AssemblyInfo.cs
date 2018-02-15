6e1c2cb7f5f92685b70605a46a7aca566d62ff30
Roman Kopaev <roman@kopaev.ru>
2018-02-15 13:49:24 +0300

    no message

--- Source/Plug-in Downloader/Properties/AssemblyInfo.cs
+++ Source/Plug-in Downloader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Roman Kopaev")]
 [assembly: AssemblyProduct("PZone XrmToolBox")]
-[assembly: AssemblyCopyright("Copyright © PZone 2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 PZone")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
